764fb7c3b3ad3337fd8ff5956533c25f7e0ef489
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2023-01-24 22:01:23 +0530

    model

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import redirect, render
 from webapp.forms import UserImageForm
 from webapp.models import UploadImage
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import argparse
+from pickle import dump, load
+from .training_caption_generator import train_data
+from keras.applications.xception import Xception, preprocess_input
+from keras.utils import pad_sequences
+from keras.models import Model, load_model
 
 def home(request):
     if request.method == 'POST':
@@ -8,6 +17,57 @@ def home(request):
         if form.is_valid():
             form.save()
             img_object = form.instance
-        return render(request, 'home.html', {'form': form, 'img_obj': img_object})
+
+            train_data()
+
+            def extract_features(filename, model):
+                try:
+                    image = Image.open(filename)
+                except:
+                    print('error')
+                image = image.resize((299, 299))
+                image = np.array(image)
+
+                if image.shape[2] == 4:
+                    image = image[..., :3]
+                image = np.expand_dims(image, axis=0)
+                image = image/127.5
+                image = image - 1.0
+                feature = model.predict(image)
+                return feature
+
+            def word_for_id(integer, tokenizer):
+                for word, index in tokenizer.word_index.items():
+                    if index == integer:
+                        return word
+                return None
+
+
+            def generate_desc(model, tokenizer, photo, max_length):
+                in_text = 'start'
+                for i in range(max_length):
+                    sequence = tokenizer.texts_to_sequences([in_text])[0]
+                    sequence = pad_sequences([sequence], maxlen = max_length)
+                    pred = model.predict([photo, sequence], verbose=0)
+                    pred = np.argmax(pred)
+                    word = word_for_id(pred, tokenizer)
+                    if word is None:
+                        break
+                    in_text += ' ' + word
+                    if word == 'end':
+                        break
+                return in_text
+                
+            max_length = 32
+            tokenizer = load(open('tokenizer.p', 'rb'))
+            model = load_model('models/model_9.h5')
+            xception_model = Xception(include_top=False, pooling='avg')
+
+            photo = extract_features(img_object.image.url, xception_model)
+            # img = Image.open(img_object.image.url)
+
+            description = generate_desc(model, tokenizer, photo, max_length)
+
+        return render(request, 'home.html', {'form': form, 'img_obj': img_object, 'description': description})
     form = UserImageForm()
     return render(request, 'home.html', {'form': form})
