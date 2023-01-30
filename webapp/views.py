c7bc573ed47bd10e7a57fcc16e294946820a73d6
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2023-01-30 15:00:29 +0530

    updated model

--- webapp/views.py
+++ webapp/views.py
@@ -11,62 +11,116 @@ from keras.applications.xception import Xception, preprocess_input
 from keras.utils import pad_sequences
 from keras.models import Model, load_model
 
+def extract_features(filename, model):
+    try:
+        image = Image.open(filename)
+    except:
+        print('error')
+        return None
+    image = image.resize((299, 299))
+    image = np.array(image)
+
+    if image.shape[2] == 4:
+        image = image[..., :3]
+    image = np.expand_dims(image, axis=0)
+    image = image/127.5
+    image = image - 1.0
+    feature = model.predict(image)
+    return feature
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen = max_length)
+        pred = model.predict([photo, sequence], verbose=0)
+        pred = np.argmax(pred)
+        word = word_for_id(pred, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
+
+def generate_caption(filename):
+    max_length = 32
+    tokenizer = load(open('tokenizer.pkl', 'rb'))
+    model = load_model('models/model_9.h5')
+    xception_model = Xception(include_top=False, pooling='avg')
+    # img_path = 'media/images' + '/' + filename
+    # print(img_path)
+    photo = extract_features(filename, xception_model)
+    img = Image.open(filename)
+    description = generate_desc(model, tokenizer, photo, max_length)
+    return description
+
 def home(request):
+    description = None
     if request.method == 'POST':
         form = UserImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            img_object = form.instance
 
-            train_data()
+            img_object = request.FILES['image'].name
+            filename = 'media/images' + '/' + img_object
+            description = generate_caption(filename)
+            # max_length = 32
+            # tokenizer = load(open('tokenizer.p', 'rb'))
+            # model = load_model('models/model_9.h5')
+            # xception_model = Xception(include_top=False, pooling='avg')
 
-            def extract_features(filename, model):
-                try:
-                    image = Image.open(filename)
-                except:
-                    print('error')
-                image = image.resize((299, 299))
-                image = np.array(image)
+            # photo = extract_features(img_object.image.name, xception_model)
+            # # img = Image.open(img_object.image.url)
 
-                if image.shape[2] == 4:
-                    image = image[..., :3]
-                image = np.expand_dims(image, axis=0)
-                image = image/127.5
-                image = image - 1.0
-                feature = model.predict(image)
-                return feature
+            # description = generate_desc(model, tokenizer, photo, max_length)
 
-            def word_for_id(integer, tokenizer):
-                for word, index in tokenizer.word_index.items():
-                    if index == integer:
-                        return word
-                return None
+            # def extract_features(filename, model):
+            #     try:
+            #         image = Image.open(filename)
+            #     except:
+            #         print('error')
+            #     image = image.resize((299, 299))
+            #     image = np.array(image)
+
+            #     if image.shape[2] == 4:
+            #         image = image[..., :3]
+            #     image = np.expand_dims(image, axis=0)
+            #     image = image/127.5
+            #     image = image - 1.0
+            #     feature = model.predict(image)
+            #     return feature
+
+            # def word_for_id(integer, tokenizer):
+            #     for word, index in tokenizer.word_index.items():
+            #         if index == integer:
+            #             return word
+            #     return None
 
 
-            def generate_desc(model, tokenizer, photo, max_length):
-                in_text = 'start'
-                for i in range(max_length):
-                    sequence = tokenizer.texts_to_sequences([in_text])[0]
-                    sequence = pad_sequences([sequence], maxlen = max_length)
-                    pred = model.predict([photo, sequence], verbose=0)
-                    pred = np.argmax(pred)
-                    word = word_for_id(pred, tokenizer)
-                    if word is None:
-                        break
-                    in_text += ' ' + word
-                    if word == 'end':
-                        break
-                return in_text
+            # def generate_desc(model, tokenizer, photo, max_length):
+            #     in_text = 'start'
+            #     for i in range(max_length):
+            #         sequence = tokenizer.texts_to_sequences([in_text])[0]
+            #         sequence = pad_sequences([sequence], maxlen = max_length)
+            #         pred = model.predict([photo, sequence], verbose=0)
+            #         pred = np.argmax(pred)
+            #         word = word_for_id(pred, tokenizer)
+            #         if word is None:
+            #             break
+            #         in_text += ' ' + word
+            #         if word == 'end':
+            #             break
+            #     return in_text
                 
-            max_length = 32
-            tokenizer = load(open('tokenizer.p', 'rb'))
-            model = load_model('models/model_9.h5')
-            xception_model = Xception(include_top=False, pooling='avg')
-
-            photo = extract_features(img_object.image.url, xception_model)
-            # img = Image.open(img_object.image.url)
-
-            description = generate_desc(model, tokenizer, photo, max_length)
+                
 
         return render(request, 'home.html', {'form': form, 'img_obj': img_object, 'description': description})
     form = UserImageForm()
